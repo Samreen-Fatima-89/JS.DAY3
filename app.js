71bd0b292b85aac718f2ccbb198680462d18d447
Samreen Fatima <irfankhans786787789@gmail.com>
2024-09-29 17:24:14 +0500

    completed

---
+++ app.js
@@ -0,0 +1,14 @@
+var num1 = +prompt("Enter your name")
+var opt = prompt("Enter your num")
+var num2 = +prompt("Enter your name")
+var result;
+if (opt == "+"){
+    result = num1 + num2;
+}
+if (opt == "-"){
+    result = num1 + num2;
+
+}if (opt == "*"){
+    result = num1 + num2;
+}
+alert(result)
